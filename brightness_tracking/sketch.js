705bfefe7ac75eda79d2c4caca3009c9488bdaad
cshall1 <61982134+cshall1@users.noreply.github.com>
2021-10-17 19:51:47 -0500

    Add files for initial brightness tracking

---
+++ brightness_tracking/sketch.js
@@ -0,0 +1,56 @@
+/**
+ * Based on the Processing Video Brightness Tracking example
+ * by Golan Levin.
+ *
+ * Tracks the brightest pixel in a live video signal.
+ */
+
+// p5.js Video capture
+let myCapture;
+// OpenCV capture helper
+let myCVCapture;
+// (RGBA) Mat to store the latest color camera frame
+let myMat;
+// Mat to store the grayscale converted camera frame
+let myMatGrayscale;
+
+function setup() {
+  createCanvas(320, 240);
+  // setup p5 capture
+  myCapture = createCapture(VIDEO);
+  myCapture.size(320, 240);
+  myCapture.hide();
+  // wait for OpenCV to init
+  p5.cv.onComplete = onOpenCVComplete;
+}
+
+function onOpenCVComplete() {
+  // create a CV capture helper
+  myCVCapture = p5.cv.getCvVideoCapture(myCapture);
+  // create a CV Mat to read new color frames into
+  myMat = p5.cv.getRGBAMat(320, 240);
+  // create a CV mat for color to grayscale conversion
+  myMatGrayscale = new cv.Mat();
+}
+
+function draw() {
+  if (p5.cv.isReady) {
+    // read from CV Capture into myMat
+    myCVCapture.read(myMat);
+    // convert Mat to grayscale
+    p5.cv.copyGray(myMat, myMatGrayscale);
+    // display Mat
+    p5.cv.drawMat(myMatGrayscale, 0, 0);
+    // get brightnest point
+    let brightestPoint = p5.cv.findMinLocation(myMatGrayscale);
+    // draw brightest point
+    circle(brightestPoint.x, brightestPoint.y, 30);
+
+    if(brightestPoint.x < 100 && brightestPoint.y < 100) {
+      print("LK1");
+    }
+
+  } else {
+    image(myCapture, 0, 0);
+  }
+}
